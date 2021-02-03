d0f4ea2fcfbb90cf67ca191faa67baaee5e4dbd2
cp2004 <31997505+cp2004@users.noreply.github.com>
2021-02-03 17:52:03 +0000

    Add bundle viewer extension code

--- source/background.js
+++ source/background.js
@@ -1,2 +1,14 @@
-// eslint-disable-next-line import/no-unassigned-import
-import './options-storage';
+browser.contextMenus.create({
+	id: "open-in-bundle-viewer",
+	title: "Open in Bundle Viewer",
+	contexts: ["link"],
+});
+browser.contextMenus.onClicked.addListener((info, tab) => {
+	if (info.menuItemId === "open-in-bundle-viewer") {
+		const url = "https://bundleviewer.octoprint.org/?url=" + encodeURIComponent(info.linkUrl)
+		let newTab = browser.tabs.create({
+			active: true,
+			url: url
+		})
+	}
+});
